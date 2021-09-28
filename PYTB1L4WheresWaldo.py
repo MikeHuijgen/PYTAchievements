24f07aed6e2f9b95b91f910784948f12d7fbf96c
Mike <28881@ma-web.nl>
2021-09-28 10:53:10 +0200

    Second commit

---
+++ PYTB1L4WheresWaldo.py
@@ -0,0 +1,16 @@
+import random
+
+people = ["Waldo","Jackson","Ray","Eric","Vaughn","Jesse","Herbert","Robert", "Rodrigo","Elija","Sasha","Nathaniel","Ellie","Allison","Jeremiah", "Paula", "Alisha","Tory","Troy", "Faye"]
+random.shuffle(people)
+
+zin = "Waldo zit op stoelnummer"
+
+print(people)
+
+index = 0
+for x in people:
+    
+    if x == "Waldo":
+        print(zin, index)
+    index+=1
+       
